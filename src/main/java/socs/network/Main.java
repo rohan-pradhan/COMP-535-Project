eba9244068f4eb83b219b04b7d480e1df45a584a
rpradh3 <rohan.pradhan@mail.mcgill.ca>
2018-01-30 18:53:31 -0500

    Initial Commit

---
+++ src/main/java/socs/network/Main.java
@@ -0,0 +1,17 @@
+package socs.network;
+
+import socs.network.node.Router;
+import socs.network.util.Configuration;
+
+public class Main {
+
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      System.out.println("usage: program conf_path");
+      System.out.println("Go chen");
+      System.exit(1);
+    }
+    Router r = new Router(new Configuration(args[0]));
+    r.terminal();
+  }
+}
